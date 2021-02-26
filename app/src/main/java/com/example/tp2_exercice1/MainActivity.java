a7837d85b850aa7df16488eb0664d5b5ecffea3e
samantha <nsamanthaduclaire@gmail.com>
2021-02-26 22:43:32 +0100

    Authentification Finished

--- app/src/main/java/com/example/tp2_exercice1/MainActivity.java
+++ app/src/main/java/com/example/tp2_exercice1/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
